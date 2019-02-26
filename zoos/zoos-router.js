8f73a6f15ed7c7b864179ee1b2613646243a6a5e
josharmantrout91 <josh.armantrout@gmail.com>
2019-02-26 18:05:30 -0500

    Successful POST and PUT methods

--- zoos/zoos-router.js
+++ zoos/zoos-router.js
@@ -40,16 +40,61 @@ router.get("/:id", (req, res) => {
 // POST a new zoo
 
 router.post("/", (req, res) => {
+  const newZoo = req.body;
   db("zoos")
-    .insert(req.body)
-    .then(zoos => {
-      res.status(201).json(zoos);
+    .insert(newZoo)
+    .then(ids => {
+      const id = [ids];
+
+      db("zoos")
+        .where({ id })
+        .first()
+        .then(zoo => {
+          res.status(201).json(zoo);
+        });
     })
     .catch(error => {
       res.status(500).json({ message: "unable to add new zoo" });
     });
 });
 
-module.exports = router;
+// UPDATE the name of an existing zoo
 
-// new pr
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  db("zoos")
+    .where({ id })
+    .update(req.body)
+    .then(count => {
+      if (count > 0) {
+        db("zoos")
+          .where({ id })
+          .then(zoo => {
+            res.status(200).json(zoo);
+          });
+      } else {
+        res.status(404).json({ message: "could not locate intended zoo" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+// DELETE a zoo
+
+router.delete("/:id", (req, res) => {
+    const id = req.params.id;
+    db("zoos")
+    .where({id})
+    .del()
+    .then(count => {
+        if (count > 0) {
+            res.status(204).json({message: "zoo successfully deleted"})
+        } else {
+            res.status(404).json({message: })
+        }
+    })
+})
+
+module.exports = router;
