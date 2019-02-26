019ef452d25033cea5c85a1a49024783d7914d1f
josharmantrout91 <josh.armantrout@gmail.com>
2019-02-26 17:04:28 -0500

    Successful GET by ID

--- zoos/zoos-router.js
+++ zoos/zoos-router.js
@@ -12,14 +12,41 @@ const knexConfig = {
 const db = knex(knexConfig);
 // endpoints here
 
+// GET our root
+
 router.get("/", (req, res) => {
-  // get the roles from the database
   db("zoos")
     .then(zoos => {
       res.status(200).json(zoos);
     })
     .catch(error => {
       res.status(500).json(error);
+    });
+});
+
+// GET zoo by ID
+
+router.get("/:id", (req, res) => {
+  db("zoos")
+    .where({ id: req.params.id })
+    .then(zoos => {
+      res.status(200).json(zoos);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+// POST a new zoo
+
+router.post("/", (req, res) => {
+  db("zoos")
+    .insert(req.body)
+    .then(zoos => {
+      res.status(201).json(zoos);
+    })
+    .catch(error => {
+      res.status(500).json({ message: "unable to add new zoo" });
     });
 });
 
